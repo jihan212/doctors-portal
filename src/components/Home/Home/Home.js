4aa205393cd36966dab77d3555c59549236ce44b
snapper <zihanjashim2121@gmail.comm>
2021-04-11 15:52:10 +0600

    doctors portal

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Blogs from '../Blogs/Blogs';
+import Doctors from '../Doctors/Doctors';
 import FeatureService from '../FeatureService/FeatureService';
 import Header from '../Header/Header';
 import MakeAppoinment from '../MakeAppoinment/MakeAppoinment';
@@ -15,6 +16,7 @@ const Home = () => {
             <MakeAppoinment></MakeAppoinment>
             <Testimonials></Testimonials>
             <Blogs></Blogs>
+            <Doctors></Doctors>
         </div>
     );
 };
