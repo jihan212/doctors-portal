1a35b881a99f3aa6b62394ba9016dad8343c7653
snapper <zihanjashim2121@gmail.comm>
2021-04-12 11:32:18 +0600

    doctor's portal

--- src/components/Appointment/AppointHeader/AppointHeader.js
+++ src/components/Appointment/AppointHeader/AppointHeader.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chair from '../../../images/chair.png';
-import './AppointHeader.css'
+import './AppointHeader.css';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
-const AppointHeader = () => {
+
+const AppointHeader = ({handleDateChange}) => {
+
+
+
     return (
         <div className="header">
              <main style={{height:'600px'}} className="row d-flex align-items-center">
             <div className="col-md-5 offset-md-1">
-                <h1>Calender</h1>
+                <h1>Appointment</h1>
+                <Calendar onChange={handleDateChange} value={new Date()} />
             </div>
             <div className="col-md-5">
                 <img src={chair} alt="" class="img-fluid" alt="Responsive image"/>
--- src/components/Appointment/Appointment/Appointment.js
+++ src/components/Appointment/Appointment/Appointment.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Navigation from '../../Shared/Navigation/Navigation';
 import AppointHeader from '../AppointHeader/AppointHeader';
 
 const Appointment = () => {
+
+    const [ selectedDate , setSelectedDate ] = useState(new Date());
+    const handleDateChange = date => {
+        console.log(date);
+        setSelectedDate(date);
+    }
+
     return (
         <div>
             <Navigation></Navigation>
-            <AppointHeader></AppointHeader>
+            <AppointHeader handleDateChange={handleDateChange}></AppointHeader>
             <Footer></Footer>
         </div>
     );
